1662e7a422b8d927297c5f19fa9779e0fd78f4cc
Miguel Angel Traslaviña Rodriguez <75137068+Matt21RR@users.noreply.github.com>
2021-04-07 13:59:32 +0200

    Archivo de ejemplo desarrollado el dia 06/03/21

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$dinero=2;
+	/*echo "<center>";
+	for ( $i = -10; $i < 10; $i ++ ){
+		if($i < 0){
+			echo"paso: <b>".$i."</b>";	
+		}else{
+			echo"paso: ".$i;
+		}
+		echo"<p>";
+	}
+	echo "</center>";
+	*/
+	switch ($dinero){
+		case 1:
+			echo"tienes para un cafe";
+			break;
+		case 2:
+			echo"tienes para dos cafes";
+			break;
+		case 3:
+			echo"tienes para un cafe y un sandwich";
+			break;
+		break;
+	}
