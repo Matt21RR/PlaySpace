9d2f31dffba43ea2ca8dd8a2ef33e0d7285687a2
Miguel Angel Traslaviña Rodriguez <75137068+Matt21RR@users.noreply.github.com>
2021-04-07 14:12:29 -0500

    Archivos del ejercicio 9:14AM - 07/04/2021

---
+++ index2.php
@@ -0,0 +1,3 @@
+<?php
+	include("packu.php");
+	sumar(negativo(1),negativo(2),negativo(3));
---
+++ packu.php
@@ -0,0 +1,21 @@
+<?php
+/*function destroyText($texto,$puerto){
+	$charPos=0;
+	while(strlen($texto) != $charPos){
+		echo ("<p style='color:blue; font-style:italic'>");
+		echo ($texto[$charPos]);
+		echo ("</p>");
+		$charPos++;
+	}
+	echo("Cantidad de caracteres en el texto: $charPos");
+	echo "<p>";
+	echo $puerto;
+	echo "</p>";
+}*/
+function negativo($numero){
+	return($numero * -1);
+}
+function sumar($a,$b,$c){
+	echo (negativo(2013));
+	echo ($a+$b+$c);
+}
