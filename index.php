a00194520d3434d7616a849cff04a2e1d1995452
Miguel Angel Traslaviña Rodriguez <75137068+Matt21RR@users.noreply.github.com>
2021-04-07 07:45:50 -0500

    Ejercivio realizado a las 7:45am del 07/04/21

--- index.php
+++ index.php
@@ -1,25 +1,26 @@
 <?php
-$dinero=2;
-	/*echo "<center>";
-	for ( $i = -10; $i < 10; $i ++ ){
-		if($i < 0){
-			echo"paso: <b>".$i."</b>";	
-		}else{
-			echo"paso: ".$i;
-		}
-		echo"<p>";
+$tamaño=0;
+$marcha=0;
+$velocidad=0;
+$velocidadpeligro=325;
+
+while($velocidad <= $velocidadpeligro){
+	$marcha++;
+	$velocidad = $marcha* 25;
+
+
+	if($tamaño>5){
+		$tamaño=0;
 	}
-	echo "</center>";
-	*/
-	switch ($dinero){
-		case 1:
-			echo"tienes para un cafe";
-			break;
-		case 2:
-			echo"tienes para dos cafes";
-			break;
-		case 3:
-			echo"tienes para un cafe y un sandwich";
-			break;
-		break;
+	$tamaño++;
+
+	echo ("<h".$tamaño.">");
+	echo ("Marcha: <b>".$marcha."</b> | Velocidad: <b>".$velocidad."km/h.</b>");
+	echo("<p>");
+	echo ("</h".$tamaño.">");
+
+
+	if($velocidad>$velocidadpeligro){
+		echo("Su vehiculo ha superado los <a style='color:red'>".$velocidadpeligro."km/h</a>");
 	}
+}
