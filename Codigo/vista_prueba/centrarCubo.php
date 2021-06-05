540f3ae55f2768a588e36f778ed3abb43d310fa8
YOY02 <81368814+YOY02@users.noreply.github.com>
2021-06-04 21:07:39 -0500

    Add files via upload

---
+++ Codigo/vista_prueba/centrarCubo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"><!---
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/unlogged.css">-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <title>Centrar</title>
+</head>
+<body class="m-0 vh-100 row justify-content-center align-items-center">
+    <div class="col-auto bg-danger p-5 text-center">
+        SERA QUE FUNCIONA?
+    </div>
+</body>
+</html>
