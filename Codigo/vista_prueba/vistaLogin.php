6a8d75e8c6bc073adcf4962407378dda7902486c
YOY02 <81368814+YOY02@users.noreply.github.com>
2021-06-08 17:33:30 -0500

    Add files via upload

---
+++ Codigo/vista_prueba/vistaLogin.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style-vJSMM.css">
+    <title>PruebaDiseñoLogin</title>
+</head>
+<body class="m-0 vh-100 row justify-content-center align-items-center text-center">
+    <div class="container p-0">
+    <!--- Contenedor (Login) -->
+        <div class="container container-form">
+        <!--- IMG -->
+            <img src="svg/title.png" class="img-fluid mx-auto my-5 d-block">
+        <!--- FORM -->
+            <form action="#">
+                <input type="text" class="form-control" placeholder="Nombre Usuario">
+                <input type="text" class="form-control my-3" placeholder="Contraseña">
+                <input type="submit" class="btn btn-IniciarSesion my-3" value="INICIAR SESIÓN">
+            </form>
+
+        <!----- Link (Olvido los Datos de la Cuenta) -->
+            <div>
+                <a href="#">¿Olvidaste la contraseña?</a><br>             
+            </div>
+        </div>
+    <!----- Link (Crear Cuenta) -->
+        <div class="mt-5">
+                <a href="#" class="linkCrearCuenta">Crear Cuenta</a>            
+        </div>
+    </div>
+</body>
+</html>
